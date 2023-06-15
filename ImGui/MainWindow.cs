6b2233de73a3d93d78b611f8a2baf809c69bb905
Jacob Brandt <brandtjacob525@gmail.com>
2023-06-15 12:43:30 -0400

    Added the ability to load DDS files via Pfim

--- ImGui/MainWindow.cs
+++ ImGui/MainWindow.cs
@@ -25,6 +25,9 @@ using ProgrimageImGui;
 using System.Buffers;
 using Progrimage.LuaDefs;
 using SixLabors.ImageSharp.Formats.Png;
+using LockedBitmapLibrary;
+using System.Drawing.Imaging;
+using SixLabors.ImageSharp.Advanced;
 
 namespace Progrimage
 {
@@ -199,12 +202,43 @@ namespace Progrimage
             try
             {
                 LayerManager manager = Program.ActiveInstance.LayerManager;
-                if (manager.Layers.Count == 0 || (manager.Layers.Count == 1 && manager.Layers[0].Image.Image is null))
+                bool setSavePath = manager.Layers.Count == 0 || (manager.Layers.Count == 1 && manager.Layers[0].Image.Image is null);
+                if (Path.GetExtension(file).ToLower() == ".dds")
+                {
+                    // Load DDS through Pfim
+                    Pfim.IImage image = Pfim.Pfimage.FromFile(file);
+
+                    if (image.Format != Pfim.ImageFormat.Rgba32) return;
+
+                    byte[] data = image.Data;
+                    Image<Argb32> dest = new(image.Width, image.Height);
+                    unsafe
+                    {
+                        for (int y = 0; y < image.Height; y++)
+                        {
+                            Span<Argb32> row = dest.DangerousGetPixelRowMemory(y).Span;
+                            for (int x = 0; x < image.Width; x++)
+                            {
+                                int src = (x + (image.Height - y - 1) * image.Width) * 4;
+                                row[x] = new Argb32(data[src + 2], data[src + 1], data[src], data[src + 3]);
+                            }
+                        }
+                    }
+                    image.Dispose();
+
+                    Program.ActiveInstance.CreateLayer(dest);
+                }
+                else
+                {
+                    // Load image normally
+                    Program.ActiveInstance.CreateLayer(Image.Load<Argb32>(file));
+                }
+
+                if (setSavePath)
                 {
                     // Set save path to this file
                     Util.SetLastSavePath(file);
                 }
-                Program.ActiveInstance.CreateLayer(Image.Load<Argb32>(file));
             }
             catch { }
         }
